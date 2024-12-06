7639edee347ebdf5a614bb5a85fdea22dba18461
Vikas <iamvikasbareth@gmail.com>
2024-12-07 01:36:45 +0530

    user model update and schema methods added

--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,87 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const JWT = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    require: true,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [2, "First name must be at least 2 characters long"],
+      maxlength: [50, "First name must not exceed 50 characters"],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxlength: [50, "Last name must not exceed 50 characters"],
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [4, "Passwrod msut be at least 4 characters long"],
+    },
+    age: {
+      type: Number,
+      min: [18, "Age must be above 18"],
+      max: [99, "Age cannot exceed 99"],
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+      message: "Gender must be of 'male','female', or 'others'",
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      match: [
+        /^(https?:\/\/.*\.(?:png|jpg|jpeg|svg|webp))$/,
+        "Photo URL must be a valid image URL",
+      ],
+    },
+    about: {
+      type: String,
+      default: "This is the default about of the user",
+      maxlength: [500, "About must not exceed 500 characters"],
+    },
+    skills: {
+      type: [String],
+      validate: {
+        validator: (skills) => Array.isArray(skills) && skills.length <= 10,
+        message: "Skills cannot exceed 10 items",
+      },
+      default: [],
+    },
   },
-  lastName: {
-    type: String,
-    require: true,
-  },
-  emailId: {
-    type: String,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.getJWT = function () {
+  const user = this;
+  const token = JWT.sign({ _id: user._id }, "SECRET@JWT@123", {
+    expiresIn: "1d",
+  });
+
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (inputPassword) {
+  const user = this;
+  const isPasswordValid = await bcrypt.compare(inputPassword, user.password);
+  return isPasswordValid;
+};
 
 module.exports = mongoose.model("User", userSchema);

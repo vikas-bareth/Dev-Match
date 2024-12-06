9ba7154f822c04c922606af7c7b8abfa5ee4d1c9
Vikas <iamvikasbareth@gmail.com>
2024-12-07 01:37:01 +0530

    signup data validator util function added

---
+++ src/utils/validation.js
@@ -0,0 +1,17 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password, gender, age } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid");
+  } else if (!emailId) {
+    throw new Error("Email is not valid!");
+  } else if (!password || !validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong Password!");
+  } else if (!age) {
+    throw new Error("Age required");
+  } else if (!gender) {
+    throw new Error("Gender is required");
+  }
+};
+
+module.exports = { validateSignUpData };

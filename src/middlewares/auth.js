d39b7041de426497e60dd412c39c3402670ece1a
Vikas <iamvikasbareth@gmail.com>
2024-12-07 01:36:07 +0530

    user auth middleware for authenticating user on request

---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+const JWT = require("jsonwebtoken");
+const User = require("../models/user");
+const userAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return req.status(401).json({ message: "Please Login again!" });
+    }
+    const decodedObject = await JWT.verify(token, "SECRET@JWT@123");
+    const _id = decodedObject?._id;
+    const user = await User.findById(_id);
+    if (!user) {
+      return req.status(404).send("User not found");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: error?.message });
+  }
+};
+
+module.exports = { userAuth };
